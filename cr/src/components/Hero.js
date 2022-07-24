122c1ee800c2a1c52d964baf281904c73e68f9f9
Meghana <meghanagb5@gmail.com>
2022-07-24 13:39:39 +0530

    navbar completed with hamburger

---
+++ cr/src/components/Hero.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./hero.css";
+
+function Hero() {
+  return (
+    <div>time stamp 27</div>
+  )
+}
+
+export default Hero
---
+++ cr/src/components/Navbar.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import "./navbar.css";
+import { FaBars, FaTimes } from "react-icons/fa";
+
+function Navbar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  return (
+    <div className="header">
+      <div className="container">
+        <h1>
+          De<span className="primary">Fi</span>
+        </h1>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Feature</a>
+          </li>
+          <li>
+            <a href="/">Earn</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+        <div className="btn-group">
+          <button className="btn">Connect Wallet</button>
+        </div>
+        <div className="hamburger" onClick={handleClick}>
+          {click ? (
+            <FaTimes size={20} />
+          ) : (
+            <FaBars size={20} style={{ color: "#333" }} />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
